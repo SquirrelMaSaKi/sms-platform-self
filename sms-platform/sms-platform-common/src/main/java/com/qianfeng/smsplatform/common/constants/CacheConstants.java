c4e2eaa16a8a7cb0c904d7c4d25ce61f236df531
Damon <853669721@qq.com>
2019-12-02 21:34:04 +0800

    Damon test 1

--- sms-platform/sms-platform-common/src/main/java/com/qianfeng/smsplatform/common/constants/CacheConstants.java
+++ sms-platform/sms-platform-common/src/main/java/com/qianfeng/smsplatform/common/constants/CacheConstants.java
@@ -1,15 +1,15 @@
 package com.qianfeng.smsplatform.common.constants;
 
 public class CacheConstants {
-    public final static String CACHE_PREFIX_CLIENT = "CLIENT:";
-    public final static String CACHE_PREFIX_PHASE = "PHASE:";
-    public final static String CACHE_PREFIX_BLACK = "BLACK:";
-    public final static String CACHE_PREFIX_CUSTOMER_FEE = "CUSTOMER_FEE:";
-    public final static String CACHE_PREFIX_ROUTER = "ROUTER:";
-    public final static String CACHE_PREFIX_DIRTYWORDS = "DIRTYWORDS:";
+    public final static String CACHE_PREFIX_CLIENT = "FIVE_CLIENT:";
+    public final static String CACHE_PREFIX_PHASE = "FIVE_PHASE:";
+    public final static String CACHE_PREFIX_BLACK = "FIVE_BLACK:";
+    public final static String CACHE_PREFIX_CUSTOMER_FEE = "FIVE_CUSTOMER_FEE:";
+    public final static String CACHE_PREFIX_ROUTER = "FIVE_ROUTER:";
+    public final static String CACHE_PREFIX_DIRTYWORDS = "FIVE_DIRTYWORDS:";
 
-    public final static String CACHE_PREFIX_SMS_LIMIT_FIVE_MINUTE = "LIMIT_5_MINUTE:";
-    public final static String CACHE_PREFIX_SMS_LIMIT_HOUR = "LIMIT_HOUR:";
-    public final static String CACHE_PREFIX_SMS_LIMIT_DAY = "LIMIT_DAY:";
+    public final static String CACHE_PREFIX_SMS_LIMIT_FIVE_MINUTE = "FIVE_LIMIT_5_MINUTE:";
+    public final static String CACHE_PREFIX_SMS_LIMIT_HOUR = "FIVE_LIMIT_HOUR:";
+    public final static String CACHE_PREFIX_SMS_LIMIT_DAY = "FIVE_LIMIT_DAY:";
 
 }
