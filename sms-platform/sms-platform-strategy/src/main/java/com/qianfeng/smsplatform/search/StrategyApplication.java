9ad4c0583ce16bb113150a0716b6c06c7027459a
PeiShaobo <360329112@qq.com>
2019-12-03 10:21:41 +0800

    小小的提交一下

--- sms-platform/sms-platform-strategy/src/main/java/com/qianfeng/smsplatform/search/StrategyApplication.java
+++ sms-platform/sms-platform-strategy/src/main/java/com/qianfeng/smsplatform/search/StrategyApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
+//@EnableFeignClients
 public class StrategyApplication implements CommandLineRunner {
 
     public static void main(String[] args) throws Exception {
