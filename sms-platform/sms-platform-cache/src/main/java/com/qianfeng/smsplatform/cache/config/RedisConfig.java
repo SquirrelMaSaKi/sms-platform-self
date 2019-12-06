54aa191d48e39cce046fe56abf1a537f22d0c585
Damon <853669721@qq.com>
2019-12-06 11:41:01 +0800

    cache finish

--- sms-platform/sms-platform-cache/src/main/java/com/qianfeng/smsplatform/cache/config/RedisConfig.java
+++ sms-platform/sms-platform-cache/src/main/java/com/qianfeng/smsplatform/cache/config/RedisConfig.java
@@ -1,4 +1,3 @@
-/*
 package com.qianfeng.smsplatform.cache.config;
 
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
@@ -8,30 +7,29 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-*/
 /**
  * @author damon
  * @Classname RedisConfig
  * @Date 2019/12/5 9:25
  * @Description TODO
- *//*
-
+ */
 @Configuration
 public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        //String类型的序列化方法
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         //使用fastjson序列化
         FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
         // value值的序列化采用fastJsonRedisSerializer
         redisTemplate.setValueSerializer(fastJsonRedisSerializer);
-        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
         // key的序列化采用StringRedisSerializer
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 }
-*/
